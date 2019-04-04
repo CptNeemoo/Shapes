16b1cf486c50ed63d13ce002fd92baca1c63348c
sergfrolov <sergey.frolov.main@gmail.com>
2019-04-04 10:20:22 +0200

    added collection and operation with it

--- src/main/java/com/cptneemoo/Main.java
+++ src/main/java/com/cptneemoo/Main.java
@@ -4,17 +4,46 @@ import com.cptneemoo.circle.Circle;
 import com.cptneemoo.square.Square;
 import com.cptneemoo.triangle.Triangle;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 public class Main {
+    private static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        Logger log = Logger.getLogger(Main.class.getName());
         Circle circle = new Circle(5);
-        log.info(Double.toString(circle.getArea()));
+        log.info(String.format("Area of circle is: %g",circle.getArea()));
         Square square = new Square(5);
-        log.info(Double.toString(square.getArea()));
+        log.info(String.format("Area of square is: %g",square.getArea()));
         Triangle triangle = new Triangle(3,4,5);
-        log.info(Double.toString(triangle.getArea()));
+        log.info(String.format("Area of triangle is: %g",triangle.getArea()));
+        ArrayList<AbstractShape> shapes = new ArrayList<>(3);
+        shapes.add(circle);
+        shapes.add(square);
+        shapes.add(triangle);
+        findMinAndMaxFromCollection(shapes);
+    }
+
+    private static void findMinAndMaxFromCollection(ArrayList<AbstractShape> shapes){
+        double min = Double.MAX_VALUE;
+        Type minType = null;
+        double max = Double.MIN_VALUE;
+        Type maxType = null;
+        for (AbstractShape shape : shapes) {
+            double shapeArea = shape.getArea();
+            if (shapeArea < min){
+                min = shapeArea;
+                minType = shape.getClass();
+            }
+            if (shapeArea > max){
+                max = shapeArea;
+                maxType = shape.getClass();
+            }
+        }
+        log.info(String.format("Minimal value of getArea() is an object with the class %s and value of %g",
+                minType,min));
+        log.info(String.format("Maximum value of getArea() is an object with the class %s and value of %g",
+                maxType,max));
     }
 }
